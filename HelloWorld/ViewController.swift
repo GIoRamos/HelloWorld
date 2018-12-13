4ad15ac6e240518fab301c280794b70e5e2fe98c
GIOVANNI GARCIA <rl200005@rlas-macair-39.rlas-116.org>
2018-12-13 11:47:58 -0600

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by GIOVANNI GARCIA on 9/17/18.
+//  Copyright © 2018 GIOVANNI GARCIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    var liverPoolLogo = #imageLiteral(resourceName: "liverpool-logo")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.image = liverPoolLogo}
+        
+    }
+
+   
+
+
+
+
+
